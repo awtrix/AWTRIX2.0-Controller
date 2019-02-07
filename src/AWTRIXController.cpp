44e18eec1570ff158c6f4bf61388d2d924b75da4
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2019-02-07 22:30:16 +0100

    Update AWTRIXController.cpp

--- src/AWTRIXController.cpp
+++ src/AWTRIXController.cpp
@@ -18,8 +18,8 @@ String version = "0.33";
 ////////////////////////////////////////////////////////////////
 ///////////////////////// Config begin /////////////////////////
 // Wifi Config
-const char *ssid = "xxxxxx";
-const char *password = "xxxxxx";
+const char *ssid = "xxxxx";
+const char *password = "xxxxx";
 char *awtrix_server = "192.168.178.39";
 
 /// LDR Config
@@ -58,55 +58,51 @@ unsigned long startTime = 0;
 unsigned long endTime = 0;
 unsigned long duration;
 
+// from http://playground.arduino.cc/Main/Utf8ascii
 // ****** UTF8-Decoder: convert UTF8-string to extended ASCII *******
 static byte c1;  // Last character buffer
 
 // Convert a single Character from UTF8 to Extended ASCII
 // Return "0" if a byte has to be ignored
 byte utf8ascii(byte ascii) {
-    if ( ascii<128 )   // Standard ASCII-set 0..0x7F handling  
-    {   c1=0; 
-        return( ascii ); 
-    }
-
-    // get previous input
-    byte last = c1;   // get last char
-    c1=ascii;         // remember actual character
-
-    switch (last)     // conversion depending on first UTF8-character
-    {   case 0xC2: return  (ascii);  break;
-        case 0xC3: return  (ascii | 0xC0);  break;
-        case 0x82: if(ascii==0xAC) return(0x80);       // special case Euro-symbol
-    }
-
-    return  (0);                                     // otherwise: return zero, if character has to be ignored
+  if ( ascii < 128 ) // Standard ASCII-set 0..0x7F handling
+  { c1 = 0;
+    return ( ascii );
+  }
+  // get previous input
+  byte last = c1;   // get last char
+  c1 = ascii;       // remember actual character
+  switch (last)     // conversion depending on first UTF8-character
+  { case 0xC2: return  (ascii);  break;
+    case 0xC3: return  (ascii | 0xC0) - 34;  break;// TomThumb extended characters off by 34
+    case 0x82: if (ascii == 0xAC) return (0x80);   // special case Euro-symbol
+  }
+  return  (0);                                     // otherwise: return zero, if character has to be ignored
 }
 
 // convert String object from UTF8 String to Extended ASCII
-String utf8ascii(String s)
-{       
-        String r="";
-        char c;
-        for (int i=0; i<s.length(); i++)
-        {
-                c = utf8ascii(s.charAt(i));
-                if (c!=0) r+=c;
-        }
-        return r;
+String utf8ascii(String s) {
+  String r = "";
+  char c;
+  for (int i = 0; i < s.length(); i++)
+  {
+    c = utf8ascii(s.charAt(i));
+    if (c != 0) r += c;
+  }
+  return r;
 }
 
 // In Place conversion UTF8-string to Extended ASCII (ASCII is shorter!)
-void utf8ascii(char* s)
-{       
-        int k=0;
-        char c;
-        for (int i=0; i<strlen(s); i++)
-        {
-                c = utf8ascii(s[i]);
-                if (c!=0) 
-                        s[k++]=c;
-        }
-        s[k]=0;
+void utf8ascii(char* s) {
+  int k = 0;
+  char c;
+  for (int i = 0; i < strlen(s); i++)
+  {
+    c = utf8ascii(s[i]);
+    if (c != 0)
+      s[k++] = c;
+  }
+  s[k] = 0;
 }
 
 
