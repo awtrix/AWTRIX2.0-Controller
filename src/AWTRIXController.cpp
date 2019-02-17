d6c2208b079c4286485c24881d21732e4e77ffdd
LuBeDa <lb@sell-e.de>
2019-02-17 10:39:27 +0100

    include awtrix-conf.h

--- src/AWTRIXController.cpp
+++ src/AWTRIXController.cpp
@@ -13,24 +13,11 @@
 #include <Fonts/TomThumb.h>
 #include <LightDependentResistor.h>
 
-String version = "0.34";
+String version = "0.35";
 
 ////////////////////////////////////////////////////////////////
 ///////////////////////// Config begin /////////////////////////
-// Wifi Config
-const char *ssid = "xxxxx";
-const char *password = "xxxxxx";
-char *awtrix_server = "192.168.178.39";
-
-/// LDR Config
-#define LDR_RESISTOR 10000 //ohms
-#define LDR_PIN A0
-#define LDR_PHOTOCELL LightDependentResistor::GL5516
-
-/// Matrix Config
-#define MATRIX_PIN D2
-//uncomment following line to use Matrixtype 2
-//#define MATRIX_MODEV2
+#include "awtrix-conf.h"
 
 ///////////////////////// Config end /////////////////////////
 //////////////////////////////////////////////////////////////
