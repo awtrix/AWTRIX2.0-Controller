123c5ba56432dd53c519d5a97b74580f59792608
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2019-03-26 22:52:47 +0100

    Update awtrix-conf.h

--- src/awtrix-conf.h
+++ src/awtrix-conf.h
@@ -1,8 +1,8 @@
 ///////////////////////// AWTRIX CONFIG /////////////////////////
 
 // Wifi Config
-const char *ssid = "Kindergarten";
-const char *password = "53825382";
+const char *ssid = "xxxx";
+const char *password = "xxxx";
 char *awtrix_server = "192.168.178.39";
 
 //#define USB_CONNECTION
